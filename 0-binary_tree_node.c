0ecd0b85ef12b95854820cb08ad9b4a54c5ff260
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-30 15:57:43 +0100

    binary tree header, create a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent of the tree
+ * @value: value of the node
+ * Return: new node.
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	newnode->n = value;
+
+	return (newnode);
+}
