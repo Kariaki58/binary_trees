dc74d6f93c1c0dce3323e955fa13acca2d8313eb
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-31 20:05:03 +0100

    checking if newnode is NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,10 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
-
-	if (parent == NULL)
-		return (NULL);
+	
 	newnode = malloc(sizeof(binary_tree_t));
+	if (!newnode)
+		return (NULL);
 	newnode->parent = parent;
 	newnode->left = NULL;
 	newnode->right = NULL;
