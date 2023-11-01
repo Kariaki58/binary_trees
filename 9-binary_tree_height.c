2dfff26160a8d56e6ded14e4f511863e5aa10f28
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-01 12:54:21 +0100

    finding the height of a treee

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_height - height of a binary tree
+ * @tree: tree
+ * Return: height of tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_height = 0, left_height = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->right)
+		right_height = binary_tree_height((binary_tree_t *)tree->left) + 1;
+	if (tree->left)
+		left_height = binary_tree_height((binary_tree_t *)tree->left) + 1;
+	if (right_height > left_height)
+		return (right_height);
+	return (left_height);
+}
