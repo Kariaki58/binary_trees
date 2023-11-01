40b112fb8401b0b081b7df3d86dee5fa3fe5d399
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-01 10:56:03 +0100

    insert to the right of a tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (newnode == NULL)
 		return (NULL);
 	if (parent == NULL)
+	{
+		free(newnode);
 		return (NULL);
+	}
 	newnode->left = NULL;
 	newnode->right = NULL;
 	newnode->n = value;
