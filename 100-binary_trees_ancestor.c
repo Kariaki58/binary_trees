ed9769aef5da2ea1e63861eebcf361074b82fc27
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-02 16:20:13 +0100

    finding ancestor, hope i don't have a bug

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_trees_ancestor - find the lowest ancestor of a binary tree
+ * @first: first node
+ * @second: second node
+ * Return: lowest ancestor.
+*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	if (!first || !second)
+		return (NULL);
+	if (second->parent == first)
+		return ((binary_tree_t *)first);
+	if (first->parent == second)
+		return ((binary_tree_t *)second);
+	if (first->parent == second->parent)
+		return (first->parent);
+	return binary_trees_ancestor(first, second->parent);
+	
+}
