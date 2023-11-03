41d3b71c44cedb61750ee4778f4a1593417a3945
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-03 10:54:06 +0100

    fixed bug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,31 +3,29 @@
 #include <stdio.h>
 
 /**
- * l_height - height of a binary tree
- * @tree: tree
- * Return: height of tree
-*/
-size_t l_height(const binary_tree_t *tree)
-{
-	size_t left_height = 0;
-
-	if (tree->left)
-		left_height = l_height(tree->left) + 1;
-	return (left_height);
-}
-
-/**
  * r_height - height of a binary tree
  * @tree: tree
  * Return: height of tree
 */
-size_t r_height(const binary_tree_t *tree)
+size_t height(const binary_tree_t *tree)
 {
-	size_t right_height = 0;
+	size_t l_height, r_height, max_height;
 	
+	if (!tree)
+		return (0);
+	if (tree->left)
+		l_height = 1 + height(tree->left);
+	else
+		l_height = 0;
 	if (tree->right)
-		right_height = r_height(tree->right) + 1;
-	return (right_height);
+		r_height = 1 + height(tree->right);
+	else
+		r_height = 0;
+	if (l_height > r_height)
+		max_height = l_height;
+	else
+		max_height = r_height;
+	return (max_height);
 }
 
 /**
@@ -37,7 +35,17 @@ size_t r_height(const binary_tree_t *tree)
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t l_height, r_height;
+
 	if (!tree)
 		return (0);
-	return (l_height(tree) - r_height(tree));
+	if (tree->left)
+		l_height = 1 + height(tree->left);
+	else
+		l_height = 0;
+	if (tree->right)
+		r_height = 1 + height(tree->right);
+	else
+		r_height = 0;
+	return (l_height - r_height);
 }
